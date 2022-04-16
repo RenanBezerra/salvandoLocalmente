363e0aa7c63cdccef9854c1dac923e6f8d84a18b
Renan Bezerra Gomes <renanbg90@gmail.com>
2022-04-16 14:35:16 -0300

    4.7
    Inserindo data de fluxo

--- src/servicos/Notas.js
+++ src/servicos/Notas.js
@@ -3,15 +3,15 @@ import { db } from "./SQLite"
 export function criaTabela() {
   db.transaction((transaction) => {
     transaction.executeSql("CREATE TABLE IF NOT EXISTS " + 
-      "Notas " +
-      "(id INTEGER PRIMARY KEY AUTOINCREMENT, titulo TEXT, categoria TEXT, texto TEXT);")
+      "Notasfluxo " +
+      "(id INTEGER PRIMARY KEY AUTOINCREMENT, titulo TEXT, categoria TEXT, texto TEXT, date DATETIME DEFAULT CURRENT_TIMESTAMP, createTime TIMESTAMP DEFAULT CURRENT_TIMESTAMP, updateTime DATE DEFAULT CURRENT_TIMESTAMP);")
   })
 }
 
 export async function adicionaNota(nota) {
   return new Promise((resolve) => {
     db.transaction((transaction) => {
-      transaction.executeSql("INSERT INTO Notas (titulo, categoria, texto) VALUES (?, ?, ?);", [nota.titulo, nota.categoria,
+      transaction.executeSql("INSERT INTO Notasfluxo (titulo, categoria, texto) VALUES (?, ?, ?);", [nota.titulo, nota.categoria,
       nota.texto], () => {
           resolve("Nota adicionada com sucesso!")
       })
@@ -23,7 +23,7 @@ export async function adicionaNota(nota) {
 export async function buscaNotas() {
   return new Promise((resolve) => {
     db.transaction((transaction) => {
-      transaction.executeSql("SELECT id, titulo, categoria, texto FROM Notas;", [], (transaction, resultado) => {
+      transaction.executeSql("SELECT * FROM Notasfluxo;", [], (transaction, resultado) => {
           resolve(resultado.rows._array)
       })
 
@@ -34,7 +34,7 @@ export async function buscaNotas() {
 export async function atualizaNota(nota) {
   return new Promise((resolve) => {
     db.transaction((transaction) => {
-      transaction.executeSql("UPDATE Notas SET titulo = ?, categoria = ?, texto = ? WHERE id = ?;", [nota.titulo, nota.categoria, nota.texto, nota.id], () => {
+      transaction.executeSql("UPDATE Notasfluxo SET titulo = ?, categoria = ?, texto = ? WHERE id = ?;", [nota.titulo, nota.categoria, nota.texto, nota.id], () => {
           resolve("Nota atualizada com sucesso!")
       })
     })
@@ -44,7 +44,7 @@ export async function atualizaNota(nota) {
 export async function removeNota(nota) {
   return new Promise((resolve) => {
     db.transaction((transaction) => {
-      transaction.executeSql("DELETE FROM Notas WHERE id = ?;", [nota.id], () => {
+      transaction.executeSql("DELETE FROM Notasfluxo WHERE id = ?;", [nota.id], () => {
           resolve("Nota removida com sucesso!")
       })
     })
@@ -54,7 +54,7 @@ export async function removeNota(nota) {
 export async function filtraPorCategoria(categoria) {
   return new Promise((resolve) => {
       db.transaction((tx) => {
-          tx.executeSql("SELECT * FROM Notas WHERE categoria = ?;", [categoria], (tx, results) => {
+          tx.executeSql("SELECT * FROM Notasfluxo WHERE categoria = ?;", [categoria], (tx, results) => {
               resolve(results.rows._array)
           })
       })
